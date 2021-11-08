5a2dae995310b0a04b5079bffe9792334410d57f
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-11-09 08:10:16 +0900

    chap: 2.3.1 enum

---
+++ src/main/kotlin/ch2/enum.kt
@@ -0,0 +1,18 @@
+package ch2
+
+fun main(args: Array<String>) {
+    println("Blue: ${Color2.BLUE.rgb()}")
+}
+
+enum class Color1 {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+}
+
+enum class Color2 (val r: Int, val g: Int, val b: Int) {
+    RED(255, 0, 0), ORANGE(255, 165, 0),
+    YELLOW(255, 255, 0), GREEN(0, 255, 0),
+    BLUE(0, 0, 255), INDIGO(75, 0, 130),
+    VIOLET(238,  130, 238);
+
+    fun rgb() = (r * 256 + g) * 256 + b
+}
