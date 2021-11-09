5b158a9630b4adc6c551b685b8930e6abe943886
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-11-10 08:42:48 +0900

    chap: 2.3.3 when 에 임의의 객체 사용

--- src/main/kotlin/ch2/when.kt
+++ src/main/kotlin/ch2/when.kt
@@ -2,10 +2,19 @@ package ch2.usingWhen
 
 import ch2.Color2
 import ch2.Color2.*
+import java.lang.Exception
 
 fun main(args: Array<String>) {
     println("Green: ${getMnemonic(GREEN)}")
     println("Orange: ${getWarmth(ORANGE)}")
+    println("RED + YELLOW = ${mix(RED, YELLOW)}")
+
+    try {
+        mix(RED, BLUE)
+    } catch (ex: Exception) {
+        assert(ex.message == "Dirty Color")
+        println("RED + BLUE = ${ex.message}")
+    }
 }
 
 fun getMnemonic(color: Color2) =
@@ -24,4 +33,12 @@ fun getWarmth(color: Color2) =
         RED, ORANGE, YELLOW -> "warm"
         GREEN -> "neutral"
         BLUE, INDIGO, VIOLET -> "cold"
+    }
+
+fun mix(c1: Color2, c2: Color2) =
+    when (val mixed = setOf(c1,c2)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty Color")
     }
