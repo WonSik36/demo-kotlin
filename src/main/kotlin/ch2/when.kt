e09025f22b9871e9ed3896a8d5caeb1d4038421a
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-11-11 00:04:40 +0900

    chap: 2.3.4 인자 없는 when 사용

--- src/main/kotlin/ch2/when.kt
+++ src/main/kotlin/ch2/when.kt
@@ -8,6 +8,7 @@ fun main(args: Array<String>) {
     println("Green: ${getMnemonic(GREEN)}")
     println("Orange: ${getWarmth(ORANGE)}")
     println("RED + YELLOW = ${mix(RED, YELLOW)}")
+    println("YELLOW + BLUE = ${mixOptimized(YELLOW, BLUE)}")
 
     try {
         mix(RED, BLUE)
@@ -40,5 +41,13 @@ fun mix(c1: Color2, c2: Color2) =
         setOf(RED, YELLOW) -> ORANGE
         setOf(YELLOW, BLUE) -> GREEN
         setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty Color")
+    }
+
+fun mixOptimized(c1: Color2, c2: Color2) =
+    when {
+        (c1 == RED && c2 == YELLOW) || (c1 == YELLOW && c2 == RED) -> ORANGE
+        (c1 == YELLOW && c2 == BLUE) || (c1 == BLUE && c2 == YELLOW) -> GREEN
+        (c1 == VIOLET && c2 == BLUE) || (c1 == BLUE && c2 == VIOLET) -> INDIGO
         else -> throw Exception("Dirty Color")
     }
