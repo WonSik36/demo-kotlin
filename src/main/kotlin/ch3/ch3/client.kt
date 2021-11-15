98fad0a29a23662cfdfdb2a89795b13d44179c8f
WonSik36 <cws0173@gmail.com>
2021-11-16 07:32:09 +0900

    chap: 3.3.0 확장 함수 시작

---
+++ src/main/kotlin/ch3/ch3/client.kt
@@ -0,0 +1,10 @@
+package ch3.ch3
+
+import ch3.ch3.extensions.firstChar
+import ch3.ch3.extensions.lastChar
+
+fun main(args: Array<String>) {
+    val str = "Kotlin"
+    println(str.firstChar())
+    println(str.lastChar())
+}
---
+++ src/main/kotlin/ch3/ch3/extensions/strings.kt
@@ -0,0 +1,4 @@
+package ch3.ch3.extensions
+
+fun String.firstChar(): Char = this.get(0)
+fun String.lastChar(): Char = get(length - 1)
