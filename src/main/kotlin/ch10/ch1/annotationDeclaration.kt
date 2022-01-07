f7b09650ab1a0b47fbf90327e337d751975cd1c3
WonSik36 <cws0173@gmail.com>
2022-01-08 08:16:41 +0900

    chap: 10.1.6 Using Class to Annotation Parameter

--- src/main/kotlin/ch10/ch1/annotationDeclaration.kt
+++ src/main/kotlin/ch10/ch1/annotationDeclaration.kt
@@ -15,8 +15,25 @@ fun main(args: Array<String>) {
     @MyAnnotation(name = "Name")
     val name = "alice"
 
+    // Using Class to Annotation Parameter
+    val person3Str = """{ "name": "alice", "company": { "name": "apple" } }"""
+    val p3 = deserialize<Person3>(person3Str)
+    println(p3)
 }
 
 @Retention  // default kotlin annotation is AnnotationRetention.RUNTIME
 @Target(AnnotationTarget.LOCAL_VARIABLE)
 annotation class MyAnnotation(val name: String)
+
+interface Company {
+    val name: String
+}
+
+data class CompanyImpl(override val name: String) : Company
+
+data class Person3 (
+    val name: String,
+    //  @Target(AnnotationTarget.PROPERTY)
+    //  annotation class DeserializeInterface(val targetClass: KClass<out Any>)
+    @DeserializeInterface(CompanyImpl::class) val company: Company
+)
