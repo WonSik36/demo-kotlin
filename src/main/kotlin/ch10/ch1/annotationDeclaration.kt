fdba6d3486bfc00680cd0de174fcfe371dd5f4eb
WonSik36 <cws0173@gmail.com>
2022-01-09 10:54:50 +0900

    chap: 10.1.7 Using Generic Class to Annotation Parameter

--- src/main/kotlin/ch10/ch1/annotationDeclaration.kt
+++ src/main/kotlin/ch10/ch1/annotationDeclaration.kt
@@ -19,6 +19,14 @@ fun main(args: Array<String>) {
     val person3Str = """{ "name": "alice", "company": { "name": "apple" } }"""
     val p3 = deserialize<Person3>(person3Str)
     println(p3)
+
+    // Using Generic Class to Annotation Parameter
+    val person4 = Person4("Bob", LocalDate.now())
+    val person4Str = serialize(person4)
+    println(person4Str)
+
+    val p4 = deserialize<Person4>(person4Str)
+    println(p4)
 }
 
 @Retention  // default kotlin annotation is AnnotationRetention.RUNTIME
@@ -37,3 +45,22 @@ data class Person3 (
     //  annotation class DeserializeInterface(val targetClass: KClass<out Any>)
     @DeserializeInterface(CompanyImpl::class) val company: Company
 )
+
+data class Person4 (
+    val name: String,
+    //  @Target(AnnotationTarget.PROPERTY)
+    //  annotation class CustomSerializer(val serializerClass: KClass<out ValueSerializer<*>>)
+    @CustomSerializer(LocalDateSerializer::class) val birthDate: LocalDate
+)
+
+class LocalDateSerializer: ValueSerializer<LocalDate> {
+    private val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+    override fun toJsonValue(value: LocalDate): Any? {
+        return value.format(dateFormat)
+    }
+
+    override fun fromJsonValue(jsonValue: Any?): LocalDate {
+        return LocalDate.parse(jsonValue as String, dateFormat)
+    }
+}
